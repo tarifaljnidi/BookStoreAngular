b527e455044efcd4231ca605e3fb799d7e7aeeda
tarifaljnidi <tarifaljnidi1986@hotmail.com>
2022-03-19 13:54:24 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl : "https://morning-plateau-63671.herokuapp.com/api/v1/books",
-   categoryUrl : "https://morning-plateau-63671.herokuapp.com/api/v1/book-category"
+  baseUrl : "https://bookstorebackendrestapi.herokuapp.com/api/v1/books",
+   categoryUrl : "https://bookstorebackendrestapi.herokuapp.com/api/v1/book-category"
 };
