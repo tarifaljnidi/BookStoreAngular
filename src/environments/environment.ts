5c5adc7e81ad0682b2daecc9287b52ebc6353550
tarifaljnidi <tarifaljnidi1986@hotmail.com>
2022-03-19 13:54:47 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  baseUrl : "http://localhost:8080/api/v1/books",
-   categoryUrl : "http://localhost:8080/api/v1/book-category"
+  baseUrl : "https://bookstorebackendrestapi.herokuapp.com/api/v1/books",
+   categoryUrl : "https://bookstorebackendrestapi.herokuapp.com/api/v1/book-category"
 };
 
 /*
